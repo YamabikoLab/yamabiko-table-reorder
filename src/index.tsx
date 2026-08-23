7cec2b68dc30c2dbc2232973cb74c80f55311750
yamabiko <88073693+YamabikoLab@users.noreply.github.com>
2026-08-24 07:38:23 +0900

    Connect column prototype from plugin entry

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
 import { addFilter } from '@wordpress/hooks';
 
+import './column-reorder/editor.scss';
+import { withColumnReorder } from './column-reorder/with-column-reorder';
 import './row-reorder/editor.scss';
 import { withTableReorder } from './row-reorder/with-table-reorder';
 
 addFilter( 'editor.BlockEdit', 'yamabiko-table-reorder/table-reorder', withTableReorder, 20 );
+addFilter(
+	'editor.BlockEdit',
+	'yamabiko-table-reorder/column-reorder',
+	withColumnReorder,
+	21
+);
