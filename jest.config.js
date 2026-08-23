bfb388c012ea40d506d0e061c742cfe1698b66b1
yamabiko <88073693+YamabikoLab@users.noreply.github.com>
2026-08-23 09:17:42 +0900

    Use wp-scripts Jest override filename

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-const defaultConfig = require( '@wordpress/scripts/config/jest-unit.config' );
-
-module.exports = {
-	...defaultConfig,
-	moduleNameMapper: {
-		...( defaultConfig.moduleNameMapper ?? {} ),
-		'^@/(.*)$': '<rootDir>/src/$1',
-	},
-};
