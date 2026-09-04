489b2eaf526ea365e5283e749d265045248b34c1
yamabiko <88073693+YamabikoLab@users.noreply.github.com>
2026-09-05 06:57:43 +0900

    revert: avoid global Jest configuration change

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-const defaultConfig = require( '@wordpress/scripts/config/jest-unit.config' );
-
-module.exports = {
-	...defaultConfig,
-	transformIgnorePatterns: [
-		'/node_modules/(?!(@preact)/)',
-		'\\.pnp\\.[^\\/]+$',
-	],
-};
