855b1dcbbff22d6f231a192dc8884c5c5c402cea
yamabiko <88073693+YamabikoLab@users.noreply.github.com>
2026-09-05 07:49:07 +0900

    Connect row reorder presentation

--- src/reorder/row-reorder/dnd.tsx
+++ src/reorder/row-reorder/dnd.tsx
@@ -19,6 +19,7 @@ import type { ReactNode } from 'react';
 
 import { rowDndInteraction, type RowDndSource } from './dnd-interaction';
 import { RowPcInput, type RowDndPointerDownHandler } from './pc-input';
+import { RowReorderPresentation } from './presentation';
 import type { RowReorderConstraints } from './table-integration';
 
 export type { RowDndPointerDownHandler } from './pc-input';
@@ -179,6 +180,7 @@ export const RowDnd = ( props: {
 			onDragMove={ onDragMove }
 			onDragEnd={ onDragEnd }
 		>
+			<RowReorderPresentation />
 			<RowPcInput
 				enabled={ enabled }
 				tableIdentity={ tableIdentity }
