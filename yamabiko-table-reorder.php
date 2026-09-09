142069024eb6062f2d7df303ad3c61b90158484e
yamabiko <88073693+YamabikoLab@users.noreply.github.com>
2026-09-10 05:56:08 +0900

    chore: prepare 0.7.0 release

--- yamabiko-table-reorder.php
+++ yamabiko-table-reorder.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Yamabiko Table Reorder
  * Description: Table reordering for supported blocks in the WordPress block editor.
- * Version: 0.6.2
+ * Version: 0.7.0
  * Requires at least: 6.8
  * Requires PHP: 8.1
  * Author: YamabikoLab
