a609372c49730b8f2eb41ed037c891eacda370fd
yamabiko <88073693+YamabikoLab@users.noreply.github.com>
2026-09-19 05:47:35 +0900

    Mark reorder form toolbar focus target

--- src/reorder/wordpress/components/toolbar.tsx
+++ src/reorder/wordpress/components/toolbar.tsx
@@ -242,6 +242,8 @@ export const ReorderModeToolbar = ( props: ReorderModeToolbarProps ) => {
 				/>
 				{ columnDndEntry }
 				<ToolbarButton
+					data-ytr-focus-target="rf-toolbar"
+					data-ytr-table-identity={ tableIdentity }
 					ref={ setRfAnchor }
 					disabled={ rfApplying }
 					icon={ formReorderIcon }
